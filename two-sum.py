089f87783a802b66848bfe888cce0180ef81c78f
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-07-23 09:20:28 -0400

    Create two-sum.py

---
+++ two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complement_d = {}
+        for index, value in enumerate(nums):
+            complement = target - value
+            if complement in complement_d.keys():
+                return [complement_d.get(complement), index]
+            else:
+                complement_d[value] = index
+                
