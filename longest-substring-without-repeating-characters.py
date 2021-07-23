56f8e2c67769007fae85025ffbf0b714413c3cb8
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-07-23 09:32:23 -0400

    Update longest-substring-without-repeating-characters.py

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -7,8 +7,12 @@ class Solution:
         ans = 0
         
         for i, char in enumerate(s):
+            
+            # The char is already in the dict and is at later index of the current_start
             if char in d and d[char] >= current_start:
+                # We restart the counting from the char after the duplicated char. (abcafedb: once a is duplicated we start again from b)
                 current_start = d[char] + 1
+                # Current length is current_char index - duplciated_char index
                 current_len = i - d[char]
                 d[char] = i
             else:
