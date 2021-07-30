9717d871c409164440b2830f3c41fa13d4c74a83
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-07-29 23:19:26 -0400

    Create fibonacci-number.py

---
+++ fibonacci-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        prev_fib = 0
+        curr_fib = 1
+        
+        if n == 0:
+          return prev_fib
+        elif n == 0:
+          return curr_fib
+        
+        while n > 1:
+          curr_fib += prev_fib
+          prev_fib = curr_fib - prev_fib
+          n -= 1
+          
+        return curr_fib
+      
