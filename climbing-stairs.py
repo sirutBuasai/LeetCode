cf7aecc0163f7ee513cfd1a4657408e19c11891d
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-07-29 23:20:35 -0400

    Update climbing-stairs.py

--- climbing-stairs.py
+++ climbing-stairs.py
@@ -1,16 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        prev_fib = 1
-        curr_fib = 1
+        prev_step = 1
+        curr_step = 1
         
         if n == 1:
-          return curr_fib
+          return curr_step
         
         while n > 1:
-          curr_fib += prev_fib
-          prev_fib = curr_fib - prev_fib
+          curr_step += prev_step
+          prev_step = curr_step - prev_step
           n -= 1
           
-        return curr_fib
+        return curr_step
         
