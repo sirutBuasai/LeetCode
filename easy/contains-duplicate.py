5d4621f9c5fbfebebeeb165c8ce5e9bfdc296dec
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-08-02 23:47:40 -0400

    Create contains-duplicate.py

---
+++ easy/contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        d = {}
+        
+        for n in nums:
+          if n in d.keys():
+            return d[n]
+          
+          else:
+            d[n] = True
+            
+        return False
+      
