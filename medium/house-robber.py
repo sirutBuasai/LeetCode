2467ade955a2ecf1898ef5adc940a3f3f219ebbb
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-10-21 11:54:49 -0400

    Update house-robber.py

--- medium/house-robber.py
+++ medium/house-robber.py
@@ -1,9 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-          return nums[0]
-        
-        elif len(nums) == 2:
+        if len(nums) <= 2:
           return max(nums)
         
         else:
@@ -15,4 +12,4 @@ class Solution:
             dp[i] = max(dp[:i-1]) + nums[i]
             
           return max(dp[-1], dp[-2])
-          
+    
