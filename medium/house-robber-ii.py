3093e51c1e2410051cbe7da3bd784da23a5479bf
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-10-21 12:54:21 -0400

    Create house-robber-ii.py

---
+++ medium/house-robber-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+          return max(nums)
+        
+        else:
+          def rob_sublist(nums):
+            if len(nums) <= 2:
+              return max(nums)
+      
+            else:
+              dp = [0 for _ in range(len(nums))]
+              dp[0] = nums[0]
+              dp[1] = nums[1]
+      
+            for i in range(2, len(dp)):
+              dp[i] = max(dp[:i-1]) + nums[i]
+        
+            return max(dp[-1], dp[-2])
+          
+          sub1 = rob_sublist(nums[:-1])
+          sub2 = rob_sublist(nums[1:])
+          
+          return max(sub1, sub2)
+        
