5d1daef8481d2e2a561349c979d1db5c6de916f3
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-10-21 11:33:37 -0400

    Create house-robber.py

---
+++ medium/house-robber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+          return nums[0]
+        
+        elif len(nums) == 2:
+          return max(nums)
+        
+        else:
+          dp = [0 for _ in range(len(nums))]
+          dp[0] = nums[0]
+          dp[1] = nums[1]
+          
+          for i in range(2, len(nums)):
+            dp[i] = max(dp[:i-1]) + nums[i]
+            
+          return max(dp[-1], dp[-2])
+          
