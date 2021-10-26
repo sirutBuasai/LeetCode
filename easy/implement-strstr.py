f4d8581be1f73c01c0a0e81a81594825ac4a4ba9
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-10-26 12:47:37 -0400

    Update implement-strstr.py

--- easy/implement-strstr.py
+++ easy/implement-strstr.py
@@ -1,12 +1,18 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        if needle:
-            if needle in haystack:
-                return haystack.index(needle)
-                
-            else:
-                return -1
-            
-        else:
-            return 0
+      h_len = len(haystack)
+      n_len = len(needle)
+      
+      if not needle:
+        return 0
+      
+      if n_len > h_len:
+        return -1
+      
+      else:
+        for i in range(h_len-n_len+1):
+          if haystack[i:i+n_len] == needle:
+            return i
           
+        return -1
+      
