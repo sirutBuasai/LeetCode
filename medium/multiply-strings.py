daa49bde4f1423b5128c6316e77bec7a2fb3d4ef
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-10-26 12:13:43 -0400

    Create multiply-strings.py

---
+++ medium/multiply-strings.py
@@ -0,0 +1,24 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+      def convert_int(string):
+        i = len(string)-1
+        digit = 1
+        n = 0
+        while i >= 0:
+          n += (digit * (ord(string[i]) - ord('0')))
+          digit *= 10
+          i -= 1
+        return n
+      
+      def convert_str(integer):
+        s = ""
+        while integer > 0:
+          s += chr(integer % 10 + ord('0'))
+          integer //= 10
+        if s == "":
+          return "0"
+        else:
+          return s[::-1]
+      
+      return convert_str(convert_int(num1) * convert_int(num2))
+        
