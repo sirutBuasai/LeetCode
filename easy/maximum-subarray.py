69145e9b14dd007e1010226fa3f63d9be55e15ee
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-10-26 13:53:52 -0400

    Update maximum-subarray.py

--- easy/maximum-subarray.py
+++ easy/maximum-subarray.py
@@ -1,13 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        temp = nums[0]
-        ans = nums[0]
+        curr = nums[0]
+        maxi = nums[0]
         
         for idx in range(1, len(nums)):
-            
-            temp = max(nums[idx], nums[idx] + temp)
-            ans = max(ans, temp)
+            curr = max(nums[idx], curr+nums[idx])
+            maxi = max(maxi, curr)
         
-        return ans
-      
+        return maxi
+    
