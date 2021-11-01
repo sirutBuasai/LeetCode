933705367fb534afb281b1c7b4851ad7ad2f1724
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-01 13:46:58 -0400

    Create reverse-integer.py

---
+++ medium/reverse-integer.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+      def math_reverse(num):
+        mult = 10**(len(str(num))-1)
+        res = 0
+        while num > 0:
+          digit = num % 10
+          res += (digit*mult)
+          num //= 10
+          mult //= 10
+          
+        return res
+      
+      if x < 10 and x > -10:
+        return x
+        
+      ans = math_reverse(abs(x))
+      if x < 0:
+        if (ans*-1) < (-2**31):
+          return 0
+        else:
+          return ans*-1
+        
+      if ans > ((2**31)-1):
+        return 0
+      else:
+        return ans
+          
