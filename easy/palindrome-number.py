29e1f992fcd89059cb8c6a20335816cdbc5306ce
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-01 13:32:35 -0400

    Create palindrome-number.py

---
+++ easy/palindrome-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+          return False
+        
+        string = str(x)
+        l = 0
+        r = len(string)-1
+        while l < r:
+          if string[l] != string[r]:
+            return False
+          l+=1
+          r-=1
+          
+        return True
+        
