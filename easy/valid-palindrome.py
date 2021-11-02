b9f4bd620fa2164dd26ab1669f4daa7a83c191fa
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-02 11:47:42 -0400

    Create valid-palindrome.py

---
+++ easy/valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        punctuation = '''!()-[]{};:'"`\, <>./?@#$%^&*_~'''
+        l = 0
+        r = len(s)-1
+        while l < r:
+          while s[l] in punctuation and l < r:
+            l += 1
+          
+          while s[r] in punctuation and l < r:
+            r -= 1
+            
+          if s[l] != s[r]:
+            return False
+          
+          else:
+            l += 1
+            r -= 1
+          
+        return True
+        
