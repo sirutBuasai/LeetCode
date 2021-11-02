20776749b051c09fd3310c81daae7815d7e04bb6
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-02 12:05:46 -0400

    Create minimum-depth-of-binary-tree.py

---
+++ easy/minimum-depth-of-binary-tree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(tree, depth):
+          if not tree or ((not tree.left) and (not tree.right)):
+            return depth
+          
+          depth += 1
+          if tree.left and tree.right:
+            return min(dfs(tree.left, depth), dfs(tree.right,depth))
+          elif not tree.left:
+            return dfs(tree.right, depth)
+          elif not tree.right:
+            return dfs(tree.left, depth)
+      
+        if not root:
+          return 0
+        else:
+          return dfs(root, 1)
+          
