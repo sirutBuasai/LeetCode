162c34badfebbe7492d163492c0800fef8e155ea
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-02 11:49:55 -0400

    Create binary-tree-preorder-traversal.py

---
+++ easy/binary-tree-preorder-traversal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def order(list1, tree):
+          if not tree:
+            return
+          list1.append(tree.val)
+          order(list1, tree.left)
+          order(list1, tree.right)
+          
+        result = []
+        order(result, root)
+        return result
+        
