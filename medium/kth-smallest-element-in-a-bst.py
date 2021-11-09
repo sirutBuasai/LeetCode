1e2af1159d9b20c2604a9b0194c6b293cc4ad35d
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-08 19:34:57 -0500

    Create kth-smallest-element-in-a-bst.py

---
+++ medium/kth-smallest-element-in-a-bst.py
@@ -0,0 +1,16 @@
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+      res = []
+      stack = []
+      curr = root
+      while (stack or curr) and len(res) < k:
+        if curr:
+          stack.append(curr)
+          curr = curr.left
+        else:
+          curr = stack.pop()
+          res.append(curr.val)
+          curr = curr.right
+          
+      return res[-1]
+      
