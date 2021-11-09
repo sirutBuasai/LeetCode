ac0ca2adba6ae8cd4111e3c6b69e0e3d613253a3
Sirut Buasai <73297481+sirutBuasai@users.noreply.github.com>
2021-11-08 20:42:41 -0500

    Create first-bad-version.py

---
+++ easy/first-bad-version.py
@@ -0,0 +1,13 @@
+class Solution:
+    def firstBadVersion(self, n):
+      l = 0
+      r = n
+      while l < r:
+        m = ((r-l)//2) + l
+        if isBadVersion(m):
+          r = m
+        else:
+          l = m+1
+      
+      return l
+      
