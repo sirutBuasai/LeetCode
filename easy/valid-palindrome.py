f2f361ebc00489c5216de1b35c1824a92b79939a
Sirut Buasai <sirutbuasai27@outlook.com>
2024-09-19 14:28:05 -0700

    update valid palindrome

--- easy/valid-palindrome.py
+++ easy/valid-palindrome.py
@@ -19,3 +19,22 @@ class Solution:
             r -= 1
 
         return True
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        l, r = 0, len(s)-1
+        while l < r:
+          while not s[l].isalnum() and l < r:
+            l += 1
+          while not s[r].isalnum() and l < r:
+            r -= 1
+
+          if s[l] != s[r]:
+            return False
+          else:
+            l += 1
+            r -= 1
+
+        return True
