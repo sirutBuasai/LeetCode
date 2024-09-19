7a89c9ec2d6957153a772c95714b3bc2f2772922
Sirut Buasai <sirutbuasai27@outlook.com>
2024-09-19 14:40:26 -0700

    update invert bt

--- easy/invert-binary-tree.py
+++ easy/invert-binary-tree.py
@@ -13,3 +13,23 @@ class Solution:
           self.invertTree(root.right)
 
         return root
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        temp = root
+        stack = [temp]
+
+        if not temp:
+          return None
+
+        while stack:
+            top = stack.pop()
+            top.left, top.right = top.right, top.left
+
+            if top.left:
+                stack.append(top.left)
+            if top.right:
+                stack.append(top.right)
+
+        return root
