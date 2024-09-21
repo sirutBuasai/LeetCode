494b589cc348904134b61f21a97ef0d5f741a18a
Sirut Buasai <sirutbuasai27@outlook.com>
2024-09-21 15:09:38 -0700

    update lowest bt

--- easy/lowest-common-ancestor-of-a-binary-search-tree.py
+++ easy/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,3 +5,19 @@ class Solution:
         elif p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
         return root
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or p.val == root.val or q.val == root.val:
+            return root
+
+        curr = root
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
+        return curr
