317f442ce40420b5385ba2f05faafaba68ba5533
sirutBuasai <sirutbuasai27@outlook.com>
2024-09-30 17:07:32 -0700

    add contain dup

--- easy/contains-duplicate.py
+++ easy/contains-duplicate.py
@@ -10,3 +10,17 @@ class Solution:
             d[n] = True
 
         return False
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+
+        for n in nums:
+          if n in s:
+            return True
+
+          else:
+            s.add(n)
+
+        return False
